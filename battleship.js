66657cdc1835f9fdcd0104dc960cdf26208cab1d
vinesto <oqvinesto@gmail.com>
2018-05-31 23:57:13 +0700

    add index board

--- battleship.js
+++ battleship.js
@@ -1,16 +1,21 @@
 // Your code here
 function battleBoard(row,col){
   var arrBoard=[]
-  var count = 0
+  var count = 1
+  var countRow = 0
+  var countAlpha = 0
+  var strAlpha = " ABCDEFGHIJ"
   for(let i=0; i<row; i++){
     let arrRow=[];
     arrBoard.push(arrRow);
     for(let j=0; j<col; j++){
-      arrRow.push(count)
-      count++
+      arrRow.push(" ")
     }
+    arrBoard[0][i]=countRow
+    countRow++
+    arrRow[0]=strAlpha[countAlpha]
+    countAlpha++
   }
-  arrBoard.push(arrRow)
+  return arrBoard
 }
-
 console.log(battleBoard(11,11))
