95319e8865d95c29093540f13bf5e3f4997d28d1
vinesto <oqvinesto@gmail.com>
2018-05-31 23:22:10 +0700

    first commit

--- battleship.js
+++ battleship.js
@@ -1,1 +1,16 @@
 // Your code here
+function battleBoard(row,col){
+  var arrBoard=[]
+  var count = 0
+  for(let i=0; i<row; i++){
+    let arrRow=[];
+    arrBoard.push(arrRow);
+    for(let j=0; j<col; j++){
+      arrRow.push(count)
+      count++
+    }
+  }
+  arrBoard.push(arrRow)
+}
+
+console.log(battleBoard(11,11))
