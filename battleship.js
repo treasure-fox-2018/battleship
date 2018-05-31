3dc0663accb56663f17b24494e7e29df75bf31a5
Rhesa Utomo <venderblitz@gmail.com>
2018-05-31 14:23:23 +0700

    first commit

--- battleship.js
+++ battleship.js
@@ -1,1 +1,34 @@
 // Your code here
+// ship data
+let ships = [
+  { name: 'Aircraft carrier', size: 5, alias: 'A' }, 
+  { name: 'Battleship', size: 4, alias: 'B' }, 
+  { name: 'Cruiser', size: 3, alias: 'C' }, 
+  { name: 'Destroyer', size: 2, alias: 'D' }
+]
+// generate board
+function generateBoard(len) {
+  let board = [];
+  for (let i = 0; i < len; i++) {
+    board.push([])
+    for (let j = 0; j < len; j++) {
+      board[i].push(' ')
+    }
+  }
+  return board;
+}
+// generate ship
+function generateShip(ships) {
+  let board = generateBoard(10);
+  for (let i = 0; i < ships.length; i++) {
+    let ship = ships[i];
+    for (let j = 0; j < ship.size; j++) {
+      board[i][j] = ship.alias;
+    }
+  }
+  return board;
+}
+// coordinates
+
+// console.log(generateBoard(10))
+console.log(generateShip(ships))
