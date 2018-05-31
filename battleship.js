3c764cc0ec80ce1fd910c62d0a3fbe2bfa5c7098
fadhilhaka <fadhilhanri@gmail.com>
2018-05-31 21:10:52 +0700

    not yet

--- battleship.js
+++ battleship.js
@@ -1,1 +1,30 @@
 // Your code here
+function box() {
+  let battleBox = []
+  let x = 0
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 20; j++) {
+      x++
+      if (j % 10 === 0) {
+        if (j % 2 === 0) {
+          battleBox.push('\n', '   ')
+        } else {
+          battleBox.push('\n', '   ')
+        }
+      } else {
+        if (j % 2 === 0) {
+          battleBox.push('   ')
+        } else {
+          battleBox.push('   ')
+        }
+      }
+      if (i % 2 === 0) {
+        battleBox.push('\n', '---')
+      }
+    }
+  }
+  battleBox.push('')
+  return battleBox.join('|')
+}
+
+console.log(box())
