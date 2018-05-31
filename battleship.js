1fdcb1645e53e3b535487546171912737b28d9d6
sumarsanaadi <sumarsana.adi@gmail.com>
2018-05-31 20:33:36 +0700

    first earlier commit just print board with array in it -sumarsanaadi

--- battleship.js
+++ battleship.js
@@ -1,1 +1,32 @@
 // Your code here
+
+var boardArr = [];
+
+for (let i = 0 ; i < 10 ; i++) {
+  if (boardArr[i] === undefined) {
+    boardArr.push([]);
+  }
+  for (let j = 0 ; j < 10 ; j++) {
+    boardArr[i].push('x');  
+  }
+}
+
+function printBoard() {
+  console.log('     A   B   C   D   E   F   G   H   I   J  ');
+  console.log('   +---------------------------------------+');
+  for (let i = 0 ; i < boardArr.length ; i++) {
+    let linePrint = " ";
+    if (i === boardArr.length - 1) {
+      linePrint = `${i+1} |`
+    } else linePrint = ` ${i+1} |`;
+    for (let j = 0 ; j < boardArr[i].length; j++) {
+      linePrint += " " + boardArr[i][j] + " |";
+    }
+    console.log(linePrint)
+    console.log('   |---|---|---|---|---|---|---|---|---|---|')
+  }
+  console.log('   +---------------------------------------+');
+
+}
+
+printBoard();
