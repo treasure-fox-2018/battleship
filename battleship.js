80f6483c1c40fff8a43565b4f73364b2b2097a86
ronifajrian <ronifajrian@gmail.com>
2018-06-01 10:42:21 +0700

    add battleship

--- battleship.js
+++ battleship.js
@@ -1,1 +1,19 @@
 // Your code here
+const row = +process.argv[2]
+const col = +process.argv[3]
+
+
+function boardGame() {
+    var dict = 'abcdefghij'
+    var board = []
+    for (let i = 0; i < row; i++) {
+        var boardCol = []
+        for (let j = 0; j < col; j++) {
+            boardCol.push(`${dict[j]}${i+1}`)
+        }
+        board.push(boardCol)
+    }
+    return board
+}
+
+console.log(boardGame());
